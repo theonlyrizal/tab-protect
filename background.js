e2282d349b538639d5b73e420ef321999f2d0c07
Rizal <84571944+theonlyrizal@users.noreply.github.com>
2025-08-13 22:57:47 +0600

    Update background.js

--- background.js
+++ background.js
@@ -1,19 +1,16 @@
-// Keep track of state per tab (fallback in memory)
+
 const tabStates = {};
 
-// Listen for messages from popup.js
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   const { tabId, active } = message;
   tabStates[tabId] = active;
 
-  // Update icon per tab
   chrome.action.setIcon({
     path: active ? 'icon-on.png' : 'icon-off.png',
     tabId: tabId,
   });
 
   if (active) {
-    // Inject content script that adds beforeunload listener
     try {
       await chrome.scripting.executeScript({
         target: { tabId: tabId },
@@ -23,14 +20,10 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
       console.error('Failed to inject content script:', e);
     }
   } else {
-    // Inject a script to remove listener by reloading the page
-    // (Because beforeunload listeners can't be cleanly removed)
     try {
       await chrome.scripting.executeScript({
         target: { tabId: tabId },
         func: () => {
-          // Remove beforeunload listener by reloading the tab silently
-          // Alternatively, you can just alert user to reload
           window.onbeforeunload = null;
         },
       });
@@ -40,7 +33,6 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   }
 });
 
-// Optional: Clean up state when tab is closed
 chrome.tabs.onRemoved.addListener((tabId) => {
   delete tabStates[tabId];
   chrome.storage.local.remove(tabId.toString());
